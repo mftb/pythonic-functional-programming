9871071956204dd794bc49e3d2f46d04ee72f198
Matheus Boy <mboy@redhat.com>
2023-05-26 15:19:20 -0300

    adding one more example

--- listcomps/list_of_lists.py
+++ listcomps/list_of_lists.py
@@ -1,10 +1,15 @@
+def generate_list_of_lists():
+    return [[x for x in range(0, 3)] for y in range(0, 3)]
+
+
 def linearize_list_of_lists(list_of_lists):
     return [element for sublist in list_of_lists for element in sublist]
 
 
 def main():
-    list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    assert linearize_list_of_lists(list_of_lists) == list(range(1, 10))
+    list_of_lists = generate_list_of_lists()
+    assert len(list_of_lists) == 3
+    assert len(linearize_list_of_lists(list_of_lists)) == 9
 
 
 if __name__ == "__main__":
